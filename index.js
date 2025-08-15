367ae593c63ecadb0b7ea131a32c431842cb6d0c
TranLong2000 <50242507+TranLong2000@users.noreply.github.com>
2025-08-15 14:54:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -767,8 +767,8 @@ app.post('/webhook-base', async (req, res) => {
 });
 
 app.listen(port, () => {
-  checkB2ValueChange();
-  setInterval(checkB2ValueChange, 5 * 60 * 1000);
+  lastInventorySum();
+  setInterval(lastInventorySum, 5 * 60 * 1000);
 });
 
 setInterval(() => {
