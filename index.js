8271bcc132f2b95c61da1fd5c0885048dc77d2bc
TranLong2000 <50242507+TranLong2000@users.noreply.github.com>
2025-08-28 21:02:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -494,7 +494,7 @@ async function getRebateValue(token) {
       headers: { Authorization: `Bearer ${token}` },
       timeout: 20000,
       params: {
-        valueRenderOption: 'UnformattedValue', // Đổi sang 'Formula' nếu muốn lấy công thức gốc FormattedValue
+        valueRenderOption: 'ToString', // Đổi sang 'Formula' nếu muốn lấy công thức gốc FormattedValue
         dateTimeRenderOption: 'FormattedString'
       }
     });
