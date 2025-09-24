a41c0dbdbc8b91d4c76f138e68f91d0cf99fac1d
TranLong2000 <50242507+TranLong2000@users.noreply.github.com>
2025-09-24 18:05:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1206,8 +1206,8 @@ cron.schedule("*/1 * * * *", async () => {
   }
 });
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
   fetchWOWBUY();
 });
 
